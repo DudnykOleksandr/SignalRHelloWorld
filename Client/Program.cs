73b752aa866d21e47d4a7645d700b68c1928fdcc
Oleksandr Dudnyk <Oleksandr_Dudnyk@epam.com>
2019-08-21 12:53:38 +0300

    init

---
+++ Client/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http.Connections;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connection = new HubConnectionBuilder()
+                .WithUrl("http://localhost:5003/test", HttpTransportType.WebSockets)
+                .Build();
+
+            do
+            {
+                connection.StartAsync().Wait();
+                Task.Delay(TimeSpan.FromMilliseconds(500));
+            } while (connection.State != HubConnectionState.Connected);
+
+
+            connection.On("testCall", () =>
+            {
+                Console.WriteLine("Hello world");
+            });
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Server/TestService.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Hosting;
+
+namespace Server
+{
+    public class TestServiсe : IHostedService, IDisposable
+    {
+
+        private const int DefaultSleepTime = 40;
+
+        private Timer timer;
+
+        private bool disposed = false;
+
+        private readonly IHubContext<TestHub> testHubContext;
+
+        public TestServiсe(IHubContext<TestHub> hubcontext)
+        {
+            this.testHubContext = hubcontext;
+        }
+
+        ~TestServiсe()
+        {
+            this.Dispose(false);
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            this.timer = new Timer(
+                this.DoWork,
+                null,
+                TimeSpan.Zero,
+                TimeSpan.FromSeconds(1));
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            this.timer?.Change(Timeout.Infinite, 0);
+
+            return Task.CompletedTask;
+        }
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (this.disposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                this.timer?.Dispose();
+            }
+
+            this.disposed = true;
+        }
+
+        private void DoWork(object state)
+        {
+            this.testHubContext.Clients.All.SendAsync("testCall");
+        }
+    }
+}
